9a1ad8175c1147431843a03856edb5aea85cebae
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-06-07 16:22:42 -0300

    Mejoro la carga del layout

--- Layout.cpp
+++ Layout.cpp
@@ -8,7 +8,7 @@ layout(NULL), gold(NULL), life(NULL), mana(NULL), level(NULL) {
     layout.loadFromFile("Layout_graphics/layout_prototype2.png");
     
     this->layout = std::move(layout);
-    this->changeGold(100);
+    this->changeGold(10000000);
     this->changeLife(100,200);
     this->changeMana(50,100);
     this->changeLevel(3);
@@ -18,7 +18,7 @@ void Layout::changeGold(int gold){
     std::stringstream s;
     s << "Oro: " << gold;
     Text text(mainRenderer);
-    text.loadText(s.str(),14);
+    text.loadText(s.str(),50);
     this->gold = std::move(text);
 }
 
@@ -26,7 +26,7 @@ void Layout::changeLife(int life, int max_life){
     std::stringstream s;
     s << "Vida: " << life << "/" << max_life;
     Text text(mainRenderer);
-    text.loadText(s.str(),14);
+    text.loadText(s.str(),50);
     this->life = std::move(text);
 }
 
@@ -34,7 +34,7 @@ void Layout::changeMana(int mana, int max_mana){
     std::stringstream s;
     s << "Mana: " << mana << "/" << max_mana;
     Text text(mainRenderer);
-    text.loadText(s.str(),14);
+    text.loadText(s.str(),50);
     this->mana = std::move(text);
 }
 
@@ -42,14 +42,17 @@ void Layout::changeLevel(int level){
     std::stringstream s;
     s << "Nivel: " << level;
     Text text(mainRenderer);
-    text.loadText(s.str(),14);
+    text.loadText(s.str(),50);
     this->level = std::move(text);
 }
 
 void Layout::render(int screen_width,int screen_heigth){
-    layout.render(screen_width - layout.getWidth(),0);
-    gold.render(screen_width - gold.getWidth() * 3.3,gold.getHeight() / 3);
-    life.render(screen_width - life.getWidth() * 2.3,life.getHeight() * 1.35);
-    mana.render(screen_width - mana.getWidth() * 1.1,mana.getHeight() * 1.35); //Ver forma mas eficaz de hacer esto
-    level.render(screen_width - level.getWidth() * 1.7,level.getHeight() * 0.3);
+    SDL_Rect clip = {0,0,screen_width,screen_heigth};
+    SDL_Rect rq = {0,0,screen_width / 3,screen_heigth};
+    layout.render(screen_width - layout.getWidth(),0,&clip,&rq);
+    rq = {0,0,layout.getWidth() * 40 / 100, layout.getHeight() / 22};
+    gold.render(screen_width - layout.getWidth() * 0.95, 0, &clip,&rq);
+    life.render(screen_width - layout.getWidth() * 0.95,layout.getHeight() / 22,&clip,&rq);
+    mana.render(screen_width - layout.getWidth() * 0.95/2,layout.getHeight() / 22,&clip,&rq);
+    level.render(screen_width - layout.getWidth() * 0.95/2,0,&clip,&rq);
 }
