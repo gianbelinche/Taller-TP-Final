4e230a6f18f01aae538d19167adfb158745f30d1
komod0 <juliancrespo15@gmail.com>
2020-06-08 04:13:17 -0300

    Thread safe string queue para encolar los eventos que llegan entre frame y frame

---
+++ ProtectedStrQueue.cpp
@@ -0,0 +1,27 @@
+#include <utility>
+
+#include "ProtectedStrQueue.h"
+
+ProtectedStrQueue::ProtectedStrQueue() {}
+
+ProtectedStrQueue::~ProtectedStrQueue() {}
+
+void ProtectedStrQueue::push(std::string s) {
+  std::unique_lock<std::mutex> l(m);
+  q.push(std::move(s));
+}
+
+// Si se va a usar para algo ver de mandarle el mutex
+std::string ProtectedStrQueue::pop() {
+  std::string frontElement = q.front();
+  q.pop();
+  return frontElement;
+}
+
+void ProtectedStrQueue::empty_queue(std::vector<std::string> &events) {
+  std::unique_lock<std::mutex> l(m);
+  while (!q.empty()) {
+    events.push_back(std::move(q.front()));
+    q.pop();
+  }
+}
---
+++ ProtectedStrQueue.h
@@ -0,0 +1,29 @@
+#ifndef PROTECTED_STR_QUEUE
+#define PROTECTED_STR_QUEUE
+
+#include <mutex>
+#include <queue>
+#include <string>
+#include <vector>
+
+class ProtectedStrQueue
+{
+private:
+  std::mutex m;
+  std::queue<std::string> q;
+public:
+  ProtectedStrQueue();
+
+  ~ProtectedStrQueue();
+
+  void push(std::string s);
+
+  // Pre: La cola no esta vacia
+  std::string pop();
+
+  bool empty();
+
+  void empty_queue(std::vector<std::string> &events);
+};
+
+#endif
