83e4ef6bb2b52e3b4bfb31643dfc2cf64fcc54fe
komod0 <juliancrespo15@gmail.com>
2020-06-09 04:54:03 -0300

    Logica movimiento prueba

--- Game.h
+++ Game.h
@@ -1,20 +1,26 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <atomic>
 #include <unordered_map>
+#include <vector>
 
+#include "Entity.h"
+// #include "UserMoved.h"
 #include "PlayerTestNet.h"
 #include "ProtectedStrQueue.h"
 #include "ServerEventHandler.h"
 #include "Thread.h"
 
-class Game {
+class Game : public Thread {
 private:
   // Lista de entidades(NPCs, bichos)
+  std::vector<Entity*> entities;
   // Lista de jugadores o map
-  std::unordered_map<std::string, PlayerTestNet> onlinePlayers;
+  std::unordered_map<int, PlayerTestNet> onlinePlayers;
   ProtectedStrQueue incomingEvents;
-  ServerEventHandler handler;
+  // ServerEventHandler handler;
+  std::atomic<bool> keep_running;
 public:
   Game();
 
@@ -25,6 +31,18 @@ public:
   void loop();
 
   void processInput();
+
+  void update();
+
+  // Funciones de prueba de momento
+  void addPlayer(int id);
+
+  void addEntity();
+
+  void addIncoming(std::string s);
+private:
+  // De momento un string con la dirección, probablemente cambie
+  void movePlayer(std::string &move);
 };
 
 #endif
