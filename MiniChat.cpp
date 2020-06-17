1fd816852b7b9e8ffbeb0b52efc64e3c1b764017
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-06-17 20:36:30 -0300

    Agrego defines a minichat

--- MiniChat.cpp
+++ MiniChat.cpp
@@ -1,6 +1,16 @@
 #include "MiniChat.h"
 #include "Text.h"
 #define MESSAGES_MAX_SIZE 10
+#define MAX_LETTERS_IN_A_WORD 21
+#define MESSAGES_X (screen_w * 205 / 300)
+#define MESSAGES_Y (screen_h * 17 / 20)
+#define MESSAGES_H (screen_h / 20)
+#define TO_RENDER_WORD_X (screen_w * 205 / 300)
+#define TO_RENDER_WORD_Y (screen_h * 18 / 20)
+#define TO_RENDER_WORD_W (screen_w * to_render_word.length() / 80)
+#define TO_RENDER_WORD_H (screen_h / 20)
+
+
 
 MiniChat::MiniChat(SDL_Renderer* mainRenderer) : mainRenderer(mainRenderer){}
 
@@ -16,21 +26,21 @@ void MiniChat::renderWord(std::string to_render_word,TTF_Font* gFont,int x, int 
     Text word(mainRenderer);
     word.loadText(to_render_word,gFont);
     SDL_Rect clip = {0,0,screen_w,screen_h};
-    int width = screen_w * to_render_word.length() / 80;
-    SDL_Rect rQuad = {0,0,width,screen_h / 20};
+    int width = TO_RENDER_WORD_W;
+    SDL_Rect rQuad = {0,0,width,TO_RENDER_WORD_H};
     word.render(x,y,&clip,&rQuad);
 }
 
 void MiniChat::render(int screen_w, int screen_h,TTF_Font* gFont){
     int lines = 0;
-    int x = screen_w * 205 / 300;
-    int y = screen_h * 17 / 20;
+    int x = MESSAGES_X;
+    int y = MESSAGES_Y;
     for (int i = messages.size() - 1; i >= 0; i--){
-        std::vector<std::string> partial_strings = this->getPartialString(messages[i],21);
+        std::vector<std::string> partial_strings = this->getPartialString(messages[i],MAX_LETTERS_IN_A_WORD);
         for (int j = partial_strings.size() - 1; j >= 0;j--){
             this->renderWord(partial_strings[j],gFont,x,y,screen_w,screen_h);
             lines++;
-            y -= screen_h / 20;
+            y -= MESSAGES_H;
             if (lines > MESSAGES_MAX_SIZE) break;
         }
         if (lines > MESSAGES_MAX_SIZE) break;
@@ -39,10 +49,10 @@ void MiniChat::render(int screen_w, int screen_h,TTF_Font* gFont){
 
     if (writing_word.length() > 0){
         std::string to_render_word = writing_word;
-        if (writing_word.length() > 21){
-            to_render_word = writing_word.substr(writing_word.length() - 21);
+        if (writing_word.length() > MAX_LETTERS_IN_A_WORD){
+            to_render_word = writing_word.substr(writing_word.length() - MAX_LETTERS_IN_A_WORD);
         }
-        this->renderWord(to_render_word,gFont,screen_w * 205 / 300,screen_h * 18 / 20,screen_w,screen_h);
+        this->renderWord(to_render_word,gFont,TO_RENDER_WORD_X,TO_RENDER_WORD_Y,screen_w,screen_h);
     }
 }
 
