068af8d6029ce4a153ef6ebe4550b3d3774983c8
komod0 <juliancrespo15@gmail.com>
2020-06-30 08:01:18 -0300

    Va queriendo el server

--- server_src/Server.cpp
+++ server_src/Server.cpp
@@ -2,17 +2,30 @@
 
 #include <fstream>
 #include <iostream>
+#include <unordered_map>
 #include <utility>
 
+#include "Game.h"
+#include "resources/GameState.h"
 #include "resources/Map.h"
-
+#include "resources/Persistor.h"
 
 Server::Server(const char* port, Configuration configuration)
-    : port(port), config(std::move(configuration)) {}
+    : clientAcceptor(port),
+      config(std::move(configuration)),
+      keepAccepting(true) {}
 
 Server::~Server() {}
 
 void Server::run() {
   Map map(MAP_PATH);
-  
+  GameState world(map.getCollisionMap(), 30);
+  Game game(world);
+  Persistor persistor;
+  game.run(); // Lanza el hilo principal del juego
+
+  while (keepAccepting) { 
+    Socket peer = std::move(clientAcceptor.accept());
+    ClientHandler* cli = new ClientHandler(std::move(peer), persistor);
+  }
 }
--- server_src/Server.h
+++ server_src/Server.h
@@ -1,19 +1,24 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <atomic>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 #include "config/Configuration.h"
+#include "resources/Acceptor.h"
+#include "resources/ClientHandler.h"
 #include "resources/Thread.h"
 
 #define MAP_PATH "../map.json"
 
 class Server : public Thread {
  private:
-  const char* port;
+  Acceptor clientAcceptor;
   Configuration config;
-  
+  std::vector<ClientHandler*> clients;
+  std::atomic<bool> keepAccepting;
  public:
   Server(const char* port, Configuration configuration);
 
