791c82e6c943087b8d73dca746e73cf333fdaef2
komod0 <juliancrespo15@gmail.com>
2020-06-30 07:58:50 -0300

    Algunos retoques al persistor

---
+++ server_src/resources/Persistor.h
@@ -0,0 +1,27 @@
+#ifndef CLASS_PERSISTOR
+#define CLASS_PERSISTOR
+
+#include <fstream>
+#include <map>
+#include <string>
+#include <vector>
+
+class Persistor {
+ private:
+  std::fstream file;
+  std::unordered_map<int, int> players;
+
+ public:
+  Persistor();
+
+  ~Persistor();
+
+  void persistPlayer(std::string file_name, std::vector<uint32_t> data,
+                     int player);
+
+  std::vector<uint32_t> obtainPlayerData(std::string file_name, int player);
+
+  std::unordered_map<int, int>& getPlayersMap();
+};
+
+#endif
