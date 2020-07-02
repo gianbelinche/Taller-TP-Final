7151d471e267792eae20a585a8b6050dc199ddfc
komod0 <juliancrespo15@gmail.com>
2020-07-02 20:44:43 -0300

    Protocolo server

--- server_src/resources/ServerProtocol.h
+++ server_src/resources/ServerProtocol.h
@@ -1,17 +1,39 @@
 #ifndef SERVERPROTOCOL_H
 #define SERVERPROTOCOL_H
 
+#include <cstdint>
 #include <string>
+#include <vector>
+
+#include "Events/Event.h"
+#include "Events/EntityClick.h"
+#include "Events/MessageSent.h"
+#include "Events/UserMoved.h"
+#include "ServerEventHandler.h"
+
+#define PLAYER_MOVEMENT 0
+#define CLICK_ON_ENTITY 1
+#define CLICK_ON_INVENTORY 2
+#define COMMAND 3
+#define LOGIN 4
 
 class ServerProtocol {
 private:
-  
+  ServerEventHandler& handler;
 public:
-  ServerProtocol();
+  ServerProtocol(ServerEventHandler& eventHandl);
   
   ~ServerProtocol();
 
   void decode(std::string packedEvent);
+
+  void playerMovement(std::vector<uint32_t> event);
+
+  void entityClick(std::vector<uint32_t> event);
+
+  void inventoryClick(std::vector<uint32_t> event);
+
+  void messageSent(std::vector<uint32_t> event);
 };
 
 #endif // SERVERPROTOCOL_H
