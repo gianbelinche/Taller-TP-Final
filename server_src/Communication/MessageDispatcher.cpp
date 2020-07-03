10028699375c51e79505890d5dceaee18514dbd1
komod0 <juliancrespo15@gmail.com>
2020-07-03 01:16:10 -0300

    Thread safe message dispatcher

--- server_src/Communication/MessageDispatcher.cpp
+++ server_src/Communication/MessageDispatcher.cpp
@@ -5,16 +5,24 @@ MessageDispatcher::MessageDispatcher() {}
 MessageDispatcher::~MessageDispatcher() {}
 
 void MessageDispatcher::broadcastMessage(std::vector<uint32_t> event) {
-  for (auto &it : players) {
-    if (it.second.isEnabled()) {
-      it.second.push(event);
+  std::unique_lock<std::mutex> l(m);
+  for (auto& it : players) {
+    if (it.second->isEnabled()) {
+      it.second->push(event);
     }
   }
 }
 
 void MessageDispatcher::sendMessage(int destinyId,
                                     std::vector<uint32_t> event) {
-  if (players.find() != players.end()) {  // Sino el jugador que se desconecto
-    players[destinyId].push(event);
+  std::unique_lock<std::mutex> l(m);                             
+  if (players.find(destinyId) != players.end()) {  // El jugador se desconecto?
+    players[destinyId]->push(event);
   }
+}
+
+void MessageDispatcher::addPlayerQueue(
+    uint32_t id, ProtectedQueue<std::vector<uint32_t>>* q) {
+  std::unique_lock<std::mutex> l(m);
+  players[id] = q;
 }
--- server_src/Communication/MessageDispatcher.h
+++ server_src/Communication/MessageDispatcher.h
@@ -4,13 +4,15 @@
 #include <cstdint>
 #include <unordered_map>
 #include <vector>
+#include <mutex>
 #include <string>
 
 #include "ProtectedQueue.h"
 
 class MessageDispatcher {
 private:
-  std::unordered_map<uint32_t, ProtectedQueue<std::vector<uint32_t>>> players;
+  std::unordered_map<uint32_t, ProtectedQueue<std::vector<uint32_t>>*> players;
+  std::mutex m;
 public:
   MessageDispatcher();
 
@@ -19,6 +21,8 @@ public:
   void broadcastMessage(std::vector<uint32_t> event);
 
   void sendMessage(int destinyId, std::vector<uint32_t> event);
+
+  void addPlayerQueue(uint32_t id, ProtectedQueue<std::vector<uint32_t>>* q);
 };
 
 #endif // MESSAGEDISPATCHER_H
