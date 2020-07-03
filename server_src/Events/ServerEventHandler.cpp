e478185c59b70b35add4c409222498ff9e90ce2f
komod0 <juliancrespo15@gmail.com>
2020-07-02 22:36:38 -0300

    Event listener precario para hacer pruebas, modificaciones al handler para que lo use

---
+++ server_src/Events/ServerEventHandler.cpp
@@ -0,0 +1,81 @@
+#include "ServerEventHandler.h"
+
+#include <iostream>
+
+#include "Entities/PlayerNet.h"
+
+ServerEventHandler::ServerEventHandler(GameState& state,
+                                       ServerEventListener& eventListener)
+    : world(state),
+      listener(eventListener) {}
+
+ServerEventHandler::~ServerEventHandler() {}
+
+void ServerEventHandler::handle(UserMoved& ev) {
+  PlayerNet* player = world.getPlayer(ev.getUser());
+  if (player == nullptr) {  // Caso medio raro
+    std::cerr << "Jugador no encontrado: " << ev.getUser() << std::endl;
+    return;
+  }
+
+  if (ev.getDirection() == STOP) {
+    // Broadcast a todos y return
+  }
+
+  uint32_t direction = ev.getDirection();
+  int velocity = player->getVelocity();
+  int x = player->getX();
+  int y = player->getY();
+
+  switch (direction) {
+    case UP:
+      y -= velocity;
+      break;
+    case LEFT:
+      x -= velocity;
+      break;
+    case DOWN:
+      y += velocity;
+      break;
+    case RIGHT:
+      x += velocity;
+      break;
+    default:
+      break;
+  }
+
+  if (!world.isValidPosition(x, y)) {
+    return;
+  }
+  player->move(x, y);
+  world.playerMoved(ev.getUser(), direction);
+}
+
+void ServerEventHandler::handleUserAttack(ClickEvent& ev) {
+  PlayerNet* player = world.getPlayer(ev.getUser());
+  if (player == nullptr) {
+    std::cerr << "Jugador no encontrado: " << ev.getUser() << std::endl;
+    return;
+  }
+
+  Entity* entity = world.getEntity(ev.getDestinyEntityID());
+  if (entity == nullptr) {
+    std::cerr << "Entidad de destino no encontrada" << ev.getDestinyEntityID()
+              << std::endl;
+    return;
+  }
+  if (!world.playerCanAttack(player, entity)) {
+    std::cout << "No lo dejo atacar\n";
+    return;
+  }
+
+  int damageDealt = player->attack(entity);
+  world.playerDealtDamage(player->getId(), damageDealt);
+
+  int expGain = entity->getHitExp(player->getLevel(), damageDealt);
+  if (!entity->isAlive()) {
+    expGain += entity->getDeathExp(player->getLevel());
+  }
+  std::cout << "La experiencia ganada es de: " << expGain << "\n";
+  player->receiveExp(expGain);
+}
---
+++ server_src/Events/ServerEventListener.h
@@ -0,0 +1,45 @@
+
+#include <cstdint>
+#include <unordered_map>
+#include <vector>
+
+#include "../Communication/MessageDispatcher.h"
+
+#define PLAYER_MOVEMENT 0
+#define NPS_SPAWN 1
+#define PLAYER_SPAWN 2
+#define DROP_SPAWN 3
+#define DELETE_ENTITY 4
+#define PLAYER_STATE_CHANGE 5
+#define EQUIP_ITEM 6
+#define PRINT_MSG 7
+#define INVENTORY_COMMAND 8
+#define STAT_CHANGE 9
+#define LOGIN 10
+
+class ServerEventListener {
+ private:
+  MessageDispatcher& dispatcher;
+
+ public:
+  ServerEventListener(MessageDispatcher& msgDispatcher);
+
+  ~ServerEventListener();
+
+  void playerExpGain(int id, int gain);
+
+  void playerLeveledUp(int id);
+
+  void playerMoved(int id, uint32_t direction);  // Ver  si conviene mergearlos en uno solo
+
+  void playerDealtDamage(int id, int damage);
+
+  void playerTookDamage(int id, int damage);
+
+  void monsterMoved(int id);
+
+  void playerDied(int id);
+
+  // Jugadores que se desconectan o mobs que mueren
+  void entityDisappear(int id);
+};
