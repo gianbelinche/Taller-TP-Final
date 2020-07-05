5f243bbcf675baad303447ebac469296cc8db04e
komod0 <juliancrespo15@gmail.com>
2020-07-04 22:34:37 -0300

    Modifico clase de configuracion

---
+++ server_src/Utils/Configuration.cpp
@@ -0,0 +1,48 @@
+#include "../headers/Configuration.h"
+
+#include "json/json.h"
+
+#include <iostream>
+
+Configuration::Configuration(std::string configFile) { parse(configFile); }
+
+Configuration::~Configuration() {}
+
+void Configuration::parse(std::string configFile) {
+  file.open(configFile, std::fstream::in);
+  Json::Value json_map;
+  Json::Reader reader;
+  reader.parse(file, json_map);
+
+  for (Json::Value::const_iterator itr = json_map.begin();
+       itr != json_map.end(); itr++) {
+    Json::Value json_inside_map = *itr;
+    std::unordered_map<std::string, float> real_inside_map;
+    for (Json::Value::const_iterator internal_itr = json_inside_map.begin();
+         internal_itr != json_inside_map.end(); internal_itr++) {
+      real_inside_map[internal_itr.key().asString()] =
+          (*internal_itr).asFloat();
+    }
+    configValues[itr.key().asString()] = std::move(real_inside_map);
+  }
+}
+
+void Configuration::printConfig() {
+  for (auto& k: configValues) {
+    std::cout << k.first << std::endl;
+    for (auto& kint: k.second) {
+      std::cout << "Clav: " << kint.first << " Valor: " << kint.second << std::endl;
+    }
+  std::cout << "\n";
+  }
+}
+
+const char* Configuration::getPort() {
+  int port = configValues["config"]["port"];
+  std::string port_s = std::to_string(port);
+  return port_s.data();
+}
+
+int Configuration::getFPS() {
+  return (int)configValues["config"]["FPS"];
+}
--- server_src/headers/Configuration.h
+++ server_src/headers/Configuration.h
@@ -1,15 +1,28 @@
+#ifndef CLASS_CONFIGURATION
+#define CLASS_CONFIGURATION
+#include <fstream>
 #include <string>
 #include <unordered_map>
 
 class Configuration {
  private:
+  std::fstream file;
   std::unordered_map<std::string, std::unordered_map<std::string, float>>
-      config;
+      configValues;
 
  public:
-  explicit Configuration(const char* config_path);
+  Configuration(std::string configFile);
 
   ~Configuration();
 
+  /* Parsea el archivo de configuración */
+  void parse(std::string configFile);
+
+  void printConfig();
+
   const char* getPort();
+
+  int getFPS();
 };
+
+#endif
