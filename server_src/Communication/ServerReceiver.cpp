43782f365d59eab9b6f974e2914d335ac2956c76
komod0 <juliancrespo15@gmail.com>
2020-07-05 06:58:49 -0300

    Saco algunos prints

--- server_src/Communication/ServerReceiver.cpp
+++ server_src/Communication/ServerReceiver.cpp
@@ -15,23 +15,21 @@ ServerReceiver::~ServerReceiver() {}
 void ServerReceiver::run() {
   while (keepRunning) {
     uint32_t msgLen = receiveLen();
-    std::cout << "El largo: " << msgLen << std::endl;
     std::string msg = std::move(receiveMsg(msgLen));
-    std::cout << "msg: " << msg << std::endl;
     incomingEvents.push(std::move(msg));
   }
 }
 
 std::string ServerReceiver::receiveMsg(uint32_t len) {
   std::vector<char> msgBuff(len);
-  std::cout << "bytes recibidos msg: " << peer.recv(msgBuff.data(), len) << std::endl;
+  peer.recv(msgBuff.data(), len);
   std::string ss(msgBuff.begin(), msgBuff.end());
   return ss;
 }
 
 uint32_t ServerReceiver::receiveLen() {
   char lenBuff[4];
-  std::cout << "bytes recibidos len: " << peer.recv(lenBuff, sizeof(uint32_t)) << std::endl;
+  peer.recv(lenBuff, sizeof(uint32_t));
   uint32_t len = *((uint32_t*)lenBuff);
   return htonl(len);
 }
