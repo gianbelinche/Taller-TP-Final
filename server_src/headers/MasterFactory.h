647e44f6d5f880d879d22cdb61318361af5713d8
komod0 <juliancrespo15@gmail.com>
2020-07-05 03:18:28 -0300

    Configuracion de razas y clases y creacion comoda

--- server_src/headers/MasterFactory.h
+++ server_src/headers/MasterFactory.h
@@ -1,15 +1,40 @@
 #include <atomic>
+#include <vector>
 
+#include "Class.h"
 #include "Configuration.h"
 #include "GameState.h"
 #include "Monster.h"
 #include "MonsterType.h"
+#include "PlayerNet.h"
+#include "Race.h"
 #include "ServerEventListener.h"
 
 #define GOBLIN "goblin"
 #define SKELETON "skeleton"
 #define SPIDER "spider"
 #define ZOMBIE "zombie"
+
+#define HUMAN "human"
+#define ELF "elf"
+#define DWARF "dwarf"
+#define GNOME "gnome"
+
+#define WARRIOR "warrior"
+#define WIZARD "wizard"
+#define CLERIC "cleric"
+#define PALADIN "paladin"
+
+#define HUMAN_ID 0
+#define ELF_ID 1
+#define DWARF_ID 2
+#define GNOME_ID 3
+
+#define WARRIOR_ID 0
+#define WIZARD_ID 1
+#define CLERIC_ID 2
+#define PALADIN_ID 3
+
 
 class MasterFactory {
  private:
@@ -21,6 +46,16 @@ class MasterFactory {
   MonsterType spider;
   MonsterType skeleton;
   MonsterType zombie;
+  Race human;
+  Race elf;
+  Race dwarf;
+  Race gnome;
+  Class warrior;
+  Class wizard;
+  Class cleric;
+  Class paladin;
+  std::vector<uint32_t, Race*> races;
+  std::vector<uint32_t, Class*> classes;
 
  public:
   MasterFactory(std::atomic<uint32_t>& idCounter, Configuration& configuration,
@@ -36,5 +71,5 @@ class MasterFactory {
 
   Monster* newZombie(int x, int y);
 
-  
+  PlayerNet* createPlayer(std::vector<uint32_t>& playerData);
 };
--- server_src/resources/MasterFactory.cpp
+++ server_src/resources/MasterFactory.cpp
@@ -15,7 +15,75 @@ MasterFactory::MasterFactory(std::atomic<uint32_t>& idCounter,
                config.getValues(SKELETON)["damage"],
                config.getValues(SKELETON)["level"]),
       zombie(config.getValues(ZOMBIE)["hp"], config.getValues(ZOMBIE)["damage"],
-             config.getValues(ZOMBIE)["level"]) {}
+             config.getValues(ZOMBIE)["level"]),
+      human(HUMAN_ID, config.getValues(HUMAN)["hpFactor"],
+            config.getValues(HUMAN)["recoveryFactor"],
+            config.getValues(HUMAN)["manaFactor"],
+            config.getValues(HUMAN)["constitution"],
+            config.getValues(HUMAN)["strength"],
+            config.getValues(HUMAN)["intelligence"],
+            config.getValues(HUMAN)["agility"]),
+      elf(ELF_ID, config.getValues(ELF)["hpFactor"],
+          config.getValues(ELF)["recoveryFactor"],
+          config.getValues(ELF)["manaFactor"],
+          config.getValues(ELF)["constitution"],
+          config.getValues(ELF)["strength"],
+          config.getValues(ELF)["intelligence"],
+          config.getValues(ELF)["agility"]),
+      dwarf(DWARF_ID, config.getValues(DWARF)["hpFactor"],
+            config.getValues(DWARF)["recoveryFactor"],
+            config.getValues(DWARF)["manaFactor"],
+            config.getValues(DWARF)["constitution"],
+            config.getValues(DWARF)["strength"],
+            config.getValues(DWARF)["intelligence"],
+            config.getValues(DWARF)["agility"]),
+      gnome(GNOME_ID, config.getValues(GNOME)["hpFactor"],
+            config.getValues(GNOME)["recoveryFactor"],
+            config.getValues(GNOME)["manaFactor"],
+            config.getValues(GNOME)["constitution"],
+            config.getValues(GNOME)["strength"],
+            config.getValues(GNOME)["intelligence"],
+            config.getValues(GNOME)["agility"]),
+      warrior(WARRIOR_ID, config.getValues(WARRIOR)["hpFactor"],
+              config.getValues(WARRIOR)["manaFactor"],
+              config.getValues(WARRIOR)["meditFac"],
+              config.getValues(WARRIOR)["strengthFactor"],
+              config.getValues(WARRIOR)["constitutionFactor"],
+              config.getValues(WARRIOR)["intelligenceFactor"],
+              config.getValues(WARRIOR)["agilityFactor"]),
+      wizard(WIZARD_ID, config.getValues(WIZARD)["hpFactor"],
+             config.getValues(WIZARD)["manaFactor"],
+             config.getValues(WIZARD)["meditFac"],
+             config.getValues(WIZARD)["strengthFactor"],
+             config.getValues(WIZARD)["constitutionFactor"],
+             config.getValues(WIZARD)["intelligenceFactor"],
+             config.getValues(WIZARD)["agilityFactor"]),
+      cleric(CLERIC_ID, config.getValues(CLERIC)["hpFactor"],
+             config.getValues(CLERIC)["manaFactor"],
+             config.getValues(CLERIC)["meditFac"],
+             config.getValues(CLERIC)["strengthFactor"],
+             config.getValues(CLERIC)["constitutionFactor"],
+             config.getValues(CLERIC)["intelligenceFactor"],
+             config.getValues(CLERIC)["agilityFactor"]),
+      paladin(PALADIN_ID, config.getValues(PALADIN)["hpFactor"],
+              config.getValues(PALADIN)["manaFactor"],
+              config.getValues(PALADIN)["meditFac"],
+              config.getValues(PALADIN)["strengthFactor"],
+              config.getValues(PALADIN)["constitutionFactor"],
+              config.getValues(PALADIN)["intelligenceFactor"],
+              config.getValues(PALADIN)["agilityFactor"]) {
+  
+  races[HUMAN_ID] = &human;
+  races[ELF_ID] = &elf;
+  races[DWARF_ID] = &dwarf;
+  races[GNOME_ID] = &gnome;
+
+  classes[WARRIOR_ID] = &warrior;
+  classes[WIZARD_ID] = &wizard;
+  classes[CLERIC_ID] = &cleric;
+  classes[PALADIN_ID] = &paladin;
+
+}
 
 MasterFactory::~MasterFactory() {}
 
@@ -35,3 +103,13 @@ Monster* MasterFactory::newZombie(int x, int y) {
   return zombie.newMonster(idGenerator++, x, y, world, listener);
 }
 
+PlayerNet* MasterFactory::createPlayer(std::vector<uint32_t>& playerData) {
+  uint32_t id = playerData[0];
+  uint32_t x = playerData[1];
+  uint32_t y = playerData[2];
+  uint32_t level = playerData[3];
+  uint32_t exp = playerData[4];
+  Race* playerRace = races[playerData[5]];
+  Class* playerClass = classes[playerData[6]];
+  uint32_t gold = playerData[7];
+}
