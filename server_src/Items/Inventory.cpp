7c86f44809bedc7445ab41e8ae8cf08ae76b4155
Gianbelinche <39842759+gianbelinche@users.noreply.github.com>
2020-07-07 15:05:57 -0300

    Arreglo posiciones al scar un obj del inventario

--- server_src/Items/Inventory.cpp
+++ server_src/Items/Inventory.cpp
@@ -28,6 +28,17 @@ Item* Inventory::getItem(int slot) {
 // Pre: Hay un item en el slot
 void Inventory::removeItemAt(int slot) {
   items.erase(slot);
+  std::unordered_map<int,Item*> new_inventory;
+  for (int i = 0; i < INVENTORY_SIZE;i++){
+    if (items.find(i) != items.end()){
+      if (i < slot){
+        new_inventory[i] = items[i];
+      } else if (i > slot){
+        new_inventory[i-1] = items[i];
+      }
+    }
+  }
+  this->items = std::move(new_inventory);
 }
 
 bool Inventory::isFull() {
